1f280cfefe124239358eca075f1e4787d7341b9e
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-19 16:36:10 -0500

    Personalizando el administrador (2)

--- blog/admin.py
+++ blog/admin.py
@@ -8,6 +8,17 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published','post_categories')
+    ordering = ('author', 'published')
+    search_fields = ('title','content','author__username')
+    date_hierarchy = 'published'
+    list_filter = ('author__username','categories__name')
+
+    def post_categories(self,obj):
+        # si queremos regresar html podemos vititar el siguiente URL
+        # tml: https://stackoverflow.com/questions/47953705/how-do-i-use-allow-tags-in-django-2-0-admin
+        return ", ".join(c.name for c in obj.categories.all().order_by("name"))
+    post_categories.short_description = "Categorias"
 
 
 admin.site.register(Category, CategoryAdmin)
