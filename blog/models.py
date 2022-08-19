bf8783bd69225055d01a8edbb8ca486a749ea7e5
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-19 16:57:53 -0500

    Creando las vistas del Blog (1)

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
     content = models.TextField(verbose_name="Contenido")
     published = models.DateTimeField(verbose_name="Fecha de publicación",default=now)  # Fecha manual
-    image = models.ImageField(upload_to='media',verbose_name="Imagen", null=True, blank=True)  # Pude ser nuevo
+    image = models.ImageField(upload_to='blog',verbose_name="Imagen", null=True, blank=True)  # Pude ser nuevo
     author = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)  # models.CASCADE eliminará en cascada cuando se borre el usuario
     categories = models.ManyToManyField(Category, verbose_name="Categorias")
     created = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de creación")
--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sample/',views.blog,name="sample"),
-    path('blog/',views.blog,name="blog"),
+    path('sample/',views.home,name="sample"),
     path('contact/',views.contact,name="contact"),
     path('store/',views.store,name="store"),
     path('about/',views.about,name="about"),
