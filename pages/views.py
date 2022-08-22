cfe787d36d8982089f96dcccc827082f6cf88959
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-22 18:58:22 -0500

    Template Tags propios

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Page
 
 
-def page(request, page_id):
+def page(request, page_id, page_slug):
     kw = {
         'page': get_object_or_404(Page, id=page_id)
     }
